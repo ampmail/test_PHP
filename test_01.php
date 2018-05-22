52618aaa5ca6361ce30d36823b8c31a107ec4891
ampmail <ampmail@ukr.net>
2018-05-22 20:02:20 +0300

    first commit

---
+++ test_01.php
@@ -0,0 +1,10 @@
+<?php
+
+    function getAppartmentLocation ($appNumber = 81, $levelQty = 5, $appartmentsPerFloor = 4, $entranceNumber = 5){
+        $entrance = ceil($appNumber / ($levelQty * $appartmentsPerFloor));
+        echo "Entrance: $entrance\n";
+        $floor = ceil(($appNumber - (($entrance > 1)?(($entrance - 1) * $levelQty * $appartmentsPerFloor):(0))) / $appartmentsPerFloor);
+        echo "Floor: $floor";
+    }
+
+    getAppartmentLocation();
